03044b340c1408ea8d38a8728cbdc42a7763723b
MiguelbAngels <33427315+MiguelbAngels@users.noreply.github.com>
2019-04-29 17:21:00 -0700

    Add files via upload

--- examengeo.php
+++ examengeo.php
@@ -10,24 +10,38 @@
 
 /* `examengeo`.`examen` */
 $examen = array(
+  array('ID Examen' => '300419','Nombre Examen' => 'Examen 30/04/19','ID Maestro' => '197202294','Lugar' => 'Bulevard solidaridad','Fecha' => '2019-04-30','Hr. Inicio' => '1400','Hr. Final' => '1600','Contraseña Examen' => '17161514')
 );
 
 /* `examengeo`.`incisos` */
 $incisos = array(
+  array('ID Inciso' => '10001','Inciso' => 'El planeta explota','ID Reactivo' => '20002'),
+  array('ID Inciso' => '10002','Inciso' => 'Si y solo si hay queso','ID Reactivo' => '20001'),
+  array('ID Inciso' => '10003','Inciso' => 'De quien quiera','ID Reactivo' => '20003'),
+  array('ID Inciso' => '10004','Inciso' => 'De quien la trabaje','ID Reactivo' => '20003'),
+  array('ID Inciso' => '10005','Inciso' => 'Dif. de minerales y apl.','ID Reactivo' => '20002'),
+  array('ID Inciso' => '10006','Inciso' => 'Tierra solida y dura','ID Reactivo' => '20001')
 );
 
 /* `examengeo`.`inscripcion` */
 $inscripcion = array(
+  array('ID Alumno' => '217202294','ID Examen' => '300419','ID Maestro' => '197202294','Fecha Aplicada' => '2019-04-30','ID Respuestas' => '1')
 );
 
 /* `examengeo`.`reactivos` */
 $reactivos = array(
+  array('ID Reactivo' => '20001','Pregunta' => '¿Que es una roca?','ID Respuesta Correcta' => '10001','ID Examen' => '300419'),
+  array('ID Reactivo' => '20002','Pregunta' => '¿Diferencia entre tierra y arena?','ID Respuesta Correcta' => '10002','ID Examen' => '300419'),
+  array('ID Reactivo' => '20003','Pregunta' => '¿De quien es la tierra?','ID Respuesta Correcta' => '10003','ID Examen' => '300419')
 );
 
 /* `examengeo`.`respuestas` */
 $respuestas = array(
+  array('ID Respuestas' => '20193004','# Preguntas' => '5','1' => '1','2' => '0','3' => '1','4' => '0','5' => '1','6' => '0','7' => '0','8' => '0','9' => '0','10' => '0')
 );
 
 /* `examengeo`.`usuarios` */
 $usuarios = array(
+  array('ID' => '197202294','Nombre' => 'José Doroteo Arango Arámbula','Correo' => 'PanchoVillaEsElPatron@gmail.com','Password' => 'ElPatron','Clase' => '0','ID Examen' => '0'),
+  array('ID' => '217202294','Nombre' => 'Miguel Angel Bernal Sanchez','Correo' => 'miguel15bs@gmail.com','Password' => '290419','Clase' => '1','ID Examen' => '300419')
 );
--- examengeo.sql
+++ examengeo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-04-2019 a las 00:30:45
+-- Tiempo de generación: 30-04-2019 a las 02:19:42
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.4
 
@@ -36,8 +36,15 @@ CREATE TABLE `examen` (
   `Fecha` date NOT NULL,
   `Hr. Inicio` int(4) NOT NULL,
   `Hr. Final` int(4) NOT NULL,
-  `Contraseña Examen` int(20) NOT NULL
+  `Contraseña Examen` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `examen`
+--
+
+INSERT INTO `examen` (`ID Examen`, `Nombre Examen`, `ID Maestro`, `Lugar`, `Fecha`, `Hr. Inicio`, `Hr. Final`, `Contraseña Examen`) VALUES
+(300419, 'Examen 30/04/19', 197202294, 'Bulevard solidaridad', '2019-04-30', 1400, 1600, '17161514');
 
 -- --------------------------------------------------------
 
@@ -51,6 +58,18 @@ CREATE TABLE `incisos` (
   `ID Reactivo` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `incisos`
+--
+
+INSERT INTO `incisos` (`ID Inciso`, `Inciso`, `ID Reactivo`) VALUES
+(10001, 'El planeta explota', 20002),
+(10002, 'Si y solo si hay queso', 20001),
+(10003, 'De quien quiera', 20003),
+(10004, 'De quien la trabaje', 20003),
+(10005, 'Dif. de minerales y apl.', 20002),
+(10006, 'Tierra solida y dura', 20001);
+
 -- --------------------------------------------------------
 
 --
@@ -60,10 +79,17 @@ CREATE TABLE `incisos` (
 CREATE TABLE `inscripcion` (
   `ID Alumno` int(20) NOT NULL,
   `ID Examen` int(20) NOT NULL,
-  `Maestro` int(50) NOT NULL,
+  `ID Maestro` int(50) NOT NULL,
   `Fecha Aplicada` date NOT NULL,
-  `ID Respuesta` int(20) NOT NULL
+  `ID Respuestas` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `inscripcion`
+--
+
+INSERT INTO `inscripcion` (`ID Alumno`, `ID Examen`, `ID Maestro`, `Fecha Aplicada`, `ID Respuestas`) VALUES
+(217202294, 300419, 197202294, '2019-04-30', 1);
 
 -- --------------------------------------------------------
 
@@ -78,6 +104,15 @@ CREATE TABLE `reactivos` (
   `ID Examen` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `reactivos`
+--
+
+INSERT INTO `reactivos` (`ID Reactivo`, `Pregunta`, `ID Respuesta Correcta`, `ID Examen`) VALUES
+(20001, '¿Que es una roca?', 10001, 300419),
+(20002, '¿Diferencia entre tierra y arena?', 10002, 300419),
+(20003, '¿De quien es la tierra?', 10003, 300419);
+
 -- --------------------------------------------------------
 
 --
@@ -85,19 +120,26 @@ CREATE TABLE `reactivos` (
 --
 
 CREATE TABLE `respuestas` (
-  `ID Alumno` int(20) NOT NULL,
-  `ID Pregunta` int(20) NOT NULL,
-  `1` int(1) NOT NULL,
-  `2` int(1) NOT NULL,
-  `3` int(1) NOT NULL,
-  `4` int(1) NOT NULL,
-  `5` int(1) NOT NULL,
-  `6` int(1) NOT NULL,
-  `7` int(1) NOT NULL,
-  `8` int(1) NOT NULL,
-  `9` int(1) NOT NULL,
-  `10` int(1) NOT NULL
+  `ID Respuestas` int(20) NOT NULL,
+  `# Preguntas` int(100) NOT NULL,
+  `1` tinyint(1) NOT NULL,
+  `2` tinyint(1) NOT NULL,
+  `3` tinyint(1) NOT NULL,
+  `4` tinyint(1) NOT NULL,
+  `5` tinyint(1) NOT NULL,
+  `6` tinyint(1) NOT NULL,
+  `7` tinyint(1) NOT NULL,
+  `8` tinyint(1) NOT NULL,
+  `9` tinyint(1) NOT NULL,
+  `10` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `respuestas`
+--
+
+INSERT INTO `respuestas` (`ID Respuestas`, `# Preguntas`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`) VALUES
+(20193004, 5, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -115,6 +157,14 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`ID`, `Nombre`, `Correo`, `Password`, `Clase`, `ID Examen`) VALUES
+(197202294, 'José Doroteo Arango Arámbula', 'PanchoVillaEsElPatron@gmail.com', 'ElPatron', 0, 0),
+(217202294, 'Miguel Angel Bernal Sanchez', 'miguel15bs@gmail.com', '290419', 1, 300419);
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -125,10 +175,22 @@ ALTER TABLE `examen`
   ADD PRIMARY KEY (`ID Examen`);
 
 --
+-- Indices de la tabla `incisos`
+--
+ALTER TABLE `incisos`
+  ADD PRIMARY KEY (`ID Inciso`);
+
+--
 -- Indices de la tabla `reactivos`
 --
 ALTER TABLE `reactivos`
   ADD PRIMARY KEY (`ID Reactivo`);
+
+--
+-- Indices de la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  ADD PRIMARY KEY (`ID Respuestas`);
 
 --
 -- Indices de la tabla `usuarios`
